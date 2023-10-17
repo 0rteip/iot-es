daf4936f9664847548b6a8ea704fc445e6c05587
Pietro Ventrucci <pietro.ventrucci@studio.unibo.it>
2023-10-17 19:02:24 +0200

    Define restart time

--- assignment-01/src/time_controll.h
+++ assignment-01/src/time_controll.h
@@ -7,17 +7,11 @@
 
 #define SEC 1000000             // microseconds
 #define SEC_MILLIS 1000         // milliseconds
-#define T_SLEEP 10 * SEC_MILLIS // seconds - time to slee
+#define T_SLEEP 10 * SEC_MILLIS // seconds - time before sleep
 #define T_ONE 3 * SEC_MILLIS    // seconds - time to remain on
 #define T_TWO 2 * SEC_MILLIS    // seconds - turning off time
 #define T_THREE 10 * SEC_MILLIS // seconds - player game time
-
-#define SEC 1000000             // microseconds
-#define SEC_MILLIS 1000         // milliseconds
-#define T_SLEEP 10 * SEC_MILLIS // seconds - time to slee
-#define T_ONE 3 * SEC_MILLIS    // seconds - time to remain on
-#define T_TWO 2 * SEC_MILLIS    // seconds - turning off time
-#define T_THREE 10 * SEC_MILLIS // seconds - player game time
+#define T_RESTART 10 * SEC_MILLIS // seconds - time to restart the game
 
 /**
  * Time initialization.
